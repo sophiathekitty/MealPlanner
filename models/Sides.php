cb2389f350b5c966521ad666df2f4467b152fb32
sophia daniels <sophiathekitty@users.noreply.github.com>
2021-05-21 22:03:17 -0600

    adding sides

---
+++ models/Sides.php
@@ -0,0 +1,111 @@
+<?php
+class Sides extends clsModel {
+    private static $settings = null;
+    private static function GetInstance(){
+        if(is_null(Sides::$settings)){
+            Sides::$settings = new Sides();
+        }
+        return Sides::$settings;
+    }
+    public static function LoadSides(){
+        $settings = Sides::GetInstance();
+        return $settings->LoadAll();
+    }
+    public static function LoadSideId($id){
+        $settings = sides::GetInstance();
+        return $settings->LoadById($id);
+    }
+    public $table_name = "Recipes";
+    public $fields = [
+        [
+            'Field'=>"id",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"PRI",
+            'Default'=>"",
+            'Extra'=>"auto_increment"
+        ],[
+            'Field'=>"name",
+            'Type'=>"varchar(100)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"instructions",
+            'Type'=>"text",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"user_id",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"0",
+            'Extra'=>""
+        ],[
+            'Field'=>"schedule",
+            'Type'=>"tinyint(1)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"1",
+            'Extra'=>""
+        ],[
+            'Field'=>"photo",
+            'Type'=>"varchar(60)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"none.png",
+            'Extra'=>""
+        ],[
+            'Field'=>"cook_level",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"cook_unit",
+            'Type'=>"varchar(10)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"",
+            'Extra'=>""
+        ],[
+            'Field'=>"side_id",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"0",
+            'Extra'=>""
+        ],[
+            'Field'=>"thaw_time",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"8",
+            'Extra'=>""
+        ],[
+            'Field'=>"prep_time",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"10",
+            'Extra'=>""
+        ],[
+            'Field'=>"cook_time",
+            'Type'=>"int(11)",
+            'Null'=>"NO",
+            'Key'=>"",
+            'Default'=>"25",
+            'Extra'=>""
+        ]
+    ];
+}
+
+if(defined('VALIDATE_TABLES')){
+    clsModel::$models[] = new Sides();
+}
+?>
